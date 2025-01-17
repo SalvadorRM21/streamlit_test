4adf090589e090e8fb35a0ec02583b13e990bdbc
SalvadorRM21 <salvador.ramirez@estudiantat.upc.edu>
2025-01-17 15:33:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Streamlit app configuration
-st.set_page_config(layout="wide", page_title="Heater Dashboard")
+st.set_page_config(layout="wide", page_title="ThermoScope")
 
 # Add custom styles for background
 st.markdown(
@@ -59,7 +59,7 @@ def fetch_electricity_price(date):
     }
     return electricity_prices.get(date, "N/A")
 
-st.title("Heater Dashboard")
+st.title("ThermoScope")
 
 try:
     # Fetch current temperature
